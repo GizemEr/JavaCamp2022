72891de98b3cd39bded10a3286d8aa715a476c27
Gizem Erdoğan <gizemerdogan098@gmail.com>
2022-11-02 23:29:28 +0300

    25 Ekim Dersi

---
+++ Dersler/day4/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -0,0 +1,28 @@
+package kodlama.io.rentACar.webApi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@RestController // annotation
+@RequestMapping("/api/brands")
+public class BrandsController {// api isimlendirmelerinde çoğul kullanılır
+	private BrandService brandService;
+	
+	@Autowired
+	public BrandsController(BrandService brandService) {
+		this.brandService = brandService;
+	}
+	
+	@GetMapping("/getall")
+	public List<Brand> getAll() {
+		return brandService.getAll();
+	}
+
+}
