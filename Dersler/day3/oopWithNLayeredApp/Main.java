be2c5a0ef3da9278191b5976b3889594a1355ef2
Gizem Erdoğan <gizemerdogan098@gmail.com>
2022-11-02 23:26:16 +0300

    5 Ekim Dersi

---
+++ Dersler/day3/oopWithNLayeredApp/Main.java
@@ -0,0 +1,24 @@
+package oopWithNLayeredApp;
+
+import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.core.logging.DatabaseLogger;
+import oopWithNLayeredApp.core.logging.FileLogger;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.core.logging.MailLogger;
+import oopWithNLayeredApp.dataAccess.HibernateProductDao;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.entities.Product;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Product product1 = new Product(1, "Iphone Xr", 9000);
+		
+		Logger[] loggers = {new DatabaseLogger(), new FileLogger(), new MailLogger()};
+		
+		ProductManager productManager = new ProductManager(new HibernateProductDao(), loggers);
+		productManager.add(product1);
+
+	}
+
+}
